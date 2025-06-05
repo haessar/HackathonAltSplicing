2d2afdf8c7989bcaa1503838794dc4d5678a5b92
tobyweir <tobyweir3@gmail.com>
2025-06-05 10:58:12 +0100

    Initial Jbrowse Plugin Commit

---
+++ jbrowse_plugin/src/BedAdapter2/index.ts
@@ -0,0 +1,17 @@
+import AdapterType from '@jbrowse/core/pluggableElementTypes/AdapterType'
+
+import configSchema from './configSchema'
+
+import type PluginManager from '@jbrowse/core/PluginManager'
+
+export default function BedAdapterF(pluginManager: PluginManager) {
+  pluginManager.addAdapterType(
+    () =>
+      new AdapterType({
+        name: 'BedAdapter',
+        displayName: 'BED adapter',
+        configSchema,
+        getAdapterClass: () => import('./BedAdapter').then(r => r.default),
+      }),
+  )
+}
