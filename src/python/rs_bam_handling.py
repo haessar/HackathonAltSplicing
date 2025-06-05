80163ddf76dbf6d17ba16326e7e0376fdfafcb90
StrangeR-9 <letranger9@gmail.com>
2025-06-05 12:50:34 +0100

    Fix to the argument error

--- src/python/rs_bam_handling.py
+++ src/python/rs_bam_handling.py
@@ -3,7 +3,7 @@ import pysam.samtools
 import csv
 import os
 
-def bamTaghanding(bamFile, output = False, sortTarget = "CB", mapping = False):
+def bamTagHanding(bamFile, threadNumber = 1, output = False, sortTarget = "CB", mapping = False):
     # function takes unsorted bamFile and produces a split based upon tags
     # expected inputs:
     # bamFile: string of input file name 
@@ -18,19 +18,19 @@ def bamTaghanding(bamFile, output = False, sortTarget = "CB", mapping = False):
     # if any of tags are not present in the groupings they will be left as temp files
 
     if mapping:
-        pysam.samtools.split("-d", sortTarget, "--output-fmt", "bam", "-f", "-u", bamFile, "'temp_" + sortTarget + "%!.bam'", catch_stdout=False)
+        pysam.samtools.split(bamFile, "-d", sortTarget, "-@", str(threadNumber), "--output-fmt", "BAM", "-f", "'" + sortTarget + "%!.bam'", catch_stdout=False)
         tempFiles = []
         if mapping.type() == dict:
             if output:
                 for cellType in mapping.keys:
-                    targetList = ["temp_"  + sortTarget + tag + ".bam" for tag in mapping[cellType]]
+                    targetList = [sortTarget + tag + ".bam" for tag in mapping[cellType]]
                     tempFiles.extend[targetList]
-                    pysam.merge("-f", "-o", output + cellType + ".bam", *targetList)
+                    pysam.merge("-f", "-@", threadNumber, "-o", output + cellType + ".bam", *targetList)
             else:
                 for cellType in mapping.keys:
-                    targetList = ["temp_"  + sortTarget + tag + ".bam" for tag in mapping[cellType]]
+                    targetList = [sortTarget + tag + ".bam" for tag in mapping[cellType]]
                     tempFiles.extend[targetList]
-                    pysam.merge("-f", "-o", cellType + ".bam", *targetList)
+                    pysam.merge("-f", "-@", threadNumber,  "-o", cellType + ".bam", *targetList)
         else:
             mappingDict = {}
             with open(mapping, newline="") as mappingFile:
@@ -40,20 +40,20 @@ def bamTaghanding(bamFile, output = False, sortTarget = "CB", mapping = False):
             
             if output:
                 for cellType in mappingDict.keys:
-                    targetList = ["temp_" + sortTarget + tag + ".bam" for tag in mappingDict[cellType]]
+                    targetList = [sortTarget + tag + ".bam" for tag in mappingDict[cellType]]
                     tempFiles.extend[targetList]
-                    pysam.merge("-f", "-o", output + cellType + ".bam", *targetList)
+                    pysam.merge("-f", "-@", threadNumber, "-o", output + cellType + ".bam", *targetList)
             else:
                 for cellType in mappingDict.keys:
-                    targetList = ["temp_" + sortTarget + tag + ".bam" for tag in mappingDict[cellType]]
+                    targetList = [sortTarget + tag + ".bam" for tag in mappingDict[cellType]]
                     tempFiles.extend[targetList]
-                    pysam.merge("-f", "-o", cellType + ".bam", *targetList)
+                    pysam.merge("-f", "-@", threadNumber, "-o", cellType + ".bam", *targetList)
         for tempFile in tempFiles:
             if os.path.exists(tempFile):
                 os.remove(tempFile)
         
     else:
         if output:
-            pysam.samtools.split("-d", sortTarget, "--output-fmt", "bam", "-f", "-u", bamFile, "'" + output + "_" + sortTarget + "%!.bam'", catch_stdout=False)
+            pysam.samtools.split(bamFile, "-d", sortTarget, "-@", str(threadNumber), "--output-fmt", "BAM", "-f", "'" + output + "_" + sortTarget + "%!.bam'", catch_stdout=False)
         else:
-            pysam.samtools.split("-d", sortTarget, "--output-fmt", "bam", "-f", "-u", bamFile, "'" + sortTarget + "%!.bam'", catch_stdout=False)
+            pysam.samtools.split(bamFile, "-d", sortTarget, "-@", str(threadNumber), "--output-fmt", "BAM", "-f", "'"+ sortTarget + "%!.bam'", catch_stdout=False)
