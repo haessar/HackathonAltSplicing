c5745d219ba06d9c376bb7c2bfc81df7711ba4c6
Will Haese-Hill <wahhill@gmail.com>
2025-06-22 23:40:16 +0100

    tidy up

--- src/python/bam_manipulation/rs_bam_handling.py
+++ src/python/bam_manipulation/rs_bam_handling.py
@@ -22,9 +22,7 @@ def bamTagHandling(bamFile, threadNumber = 7, output = False, sortTarget = "CB",
     # sep is the delimiter of the file
     # output is set of files split as requested.  
     # Without mapping file split file based upon tag of form (output_) sortTarget value of tag.bam
-    # If mapping file is used temporary files will be created and deleted from complete split of the tag 
-    # final input will be of form (output_) grouping.bam 
-    # if any of tags are not present in the groupings they will be left as temp files
+    # If mapping file is used final outputs will be of form (output_) grouping.bam 
     # will place results in directory named output
     '''
     if output:
@@ -35,10 +33,7 @@ def bamTagHandling(bamFile, threadNumber = 7, output = False, sortTarget = "CB",
     if not os.path.exists(dirPath):
         os.makedirs(dirPath)
     if mapping:
-        # mappingTagsMissing = []
         presentSet = set()
-        # pysam.samtools.split(bamFile, "-d", sortTarget, "-@", str(threadNumber), "-u", dirPath + "untagged.bam", "--output-fmt", "BAM", "-f", dirPath + "%!.bam", catch_stdout=False)
-        # tempFiles = []
         if isinstance(mapping, dict):
             mappingDict = mapping.copy()
         else:
@@ -48,31 +43,7 @@ def bamTagHandling(bamFile, threadNumber = 7, output = False, sortTarget = "CB",
             for cluster in clusters:
                 mappingDict[cluster] = mappingDf.loc[mappingDf[mappingColumns[0]] == cluster][mappingColumns[1]].unique()
 
-        # count = 0
-        # with pysam.AlignmentFile(bamFile, "rb") as in_bam:
-        #     for read in in_bam:
-        #         cb_tag = read.get_tag("CB") if read.has_tag("CB") else None
-        #         if cb_tag:
-        #             filter = mappingDf[mappingDf["cell_barcode"] == cb_tag]
-        #             if not filter.empty:
-        #                 count += 1
-
         tag_to_cellType = {tag: ct for ct, tags in mappingDict.items() for tag in tags}
-
-        # for cellType, tags in mappingDict.items():
-        #     in_bam = pysam.AlignmentFile(bamFile, "rb")
-        #     outFile = cellType.replace(" ", "_") + ".bam"
-        #     if output:
-        #         outFile = output + "_" + outFile
-        #     with pysam.AlignmentFile(outFile, "wb", header=in_bam.header) as out_bam:
-        #         count = 0
-        #         for tag in tags:
-        #             for read in in_bam:
-        #                 if read.has_tag("CB") and read.get_tag("CB") == tag:
-        #                     count += 1
-        #                     out_bam.write(read)
-        #     print(f"{outFile}: {count}")
-
         with pysam.AlignmentFile(bamFile, "rb") as in_bam:
             total_reads = sum(total_count for _, _, _, total_count in in_bam.get_index_statistics())
             # We will open each cell-type BAM the first time we need it
@@ -105,19 +76,6 @@ def bamTagHandling(bamFile, threadNumber = 7, output = False, sortTarget = "CB",
         for cellType, n in counts.items():
             print(f"{cellType} : {n} reads")
         print(f"% of mapping tags found in BAM: {round(100 * len(presentSet) / len(mappingDf), 2)}")
-            # targetList = [dirPath + ("'" + tag + ".bam'")  for tag in tags]
-            # tempFiles.extend(targetList)
-            # presentList = [tag for tag in targetList if Path(tag).is_file()]
-            # mappingTagsMissing.extend(list(set(targetList)- set(presentList)))
-            # if len(presentList) > 0:
-            #     pysam.merge("-f", "-@", str(threadNumber), "-o", dirPath + outFile, *presentList)
-        # for tempFile in tempFiles:
-        #     if os.path.exists(tempFile):
-        #         os.remove(tempFile)
-        # if len(mappingTagsMissing) > 0:
-        #     mappingTagsMissing = [tagNotPresent.split("/")[-1].strip("'")[0:-4] for tagNotPresent in mappingTagsMissing]
-        #     print("Following labelled cells missing in data ")
-        #     print(mappingTagsMissing)
         
     else:
         if output:
