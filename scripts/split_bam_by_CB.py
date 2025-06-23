f29e76e5010e047731ad29365b56a23dfd17f4f5
Will Haese-Hill <wahhill@gmail.com>
2025-06-23 14:55:43 +0100

    simple script callable from SLURM

---
+++ scripts/split_bam_by_CB.py
@@ -0,0 +1,9 @@
+import sys
+
+from bam_manipulation.rs_bam_handling import bamTagHandling
+
+
+if __name__ == "__main__":
+    in_bam = sys.argv[1]
+    mapping = sys.argv[2]
+    bamTagHandling(in_bam, mapping=mapping, unmapped=True, untagged=True)
