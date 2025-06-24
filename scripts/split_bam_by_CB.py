1c66759bd8731cb3f4d0b6535b6599015e2c591e
Will Haese-Hill <wahhill@gmail.com>
2025-06-24 12:04:54 +0100

    allow adding output prefix to split_bam_by_CB

--- scripts/split_bam_by_CB.py
+++ scripts/split_bam_by_CB.py
@@ -7,4 +7,5 @@ from bam_manipulation.rs_bam_handling import bamTagHandling
 if __name__ == "__main__":
     in_bam = sys.argv[1]
     mapping = sys.argv[2]
-    bamTagHandling(in_bam, mapping=mapping, unmapped=True, untagged=True)
+    output = sys.argv[3]
+    bamTagHandling(in_bam, mapping=mapping, output=output, unmapped=True, untagged=True)
