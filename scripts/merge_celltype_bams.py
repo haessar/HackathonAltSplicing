789bf0fdca2210897207282e8b9455a280fc0508
Will Haese-Hill <wahhill@gmail.com>
2025-08-15 20:30:55 +0100

    parallelise merging for speed

--- scripts/merge_celltype_bams.py
+++ scripts/merge_celltype_bams.py
@@ -7,4 +7,4 @@ from bam_manipulation.merge_samples_by_celltype import merge_samples_by_celltype
 if __name__ == "__main__":
     input_dir = sys.argv[1]
     output_dir = sys.argv[2]
-    merge_samples_by_celltype(input_dir, output_dir)
+    merge_samples_by_celltype(input_dir, output_dir, threads=8)
--- src/python/bam_manipulation/merge_samples_by_celltype.py
+++ src/python/bam_manipulation/merge_samples_by_celltype.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pysam
 
 
-def merge_samples_by_celltype(input_dir, output_dir, samples=["Mira_1", "Mira_2", "Mira_3", "Mira_4"]):
+def merge_samples_by_celltype(input_dir, output_dir, samples=["Mira_1", "Mira_2", "Mira_3", "Mira_4"], threads=1):
     celltypes = set()
     for sample in samples:
         p = Path(input_dir) / f"{sample}_files"
@@ -14,4 +14,4 @@ def merge_samples_by_celltype(input_dir, output_dir, samples=["Mira_1", "Mira_2"
     output_dir.mkdir(parents=True, exist_ok=True)
     for ct in celltypes:
         bam_files = Path(input_dir).glob(f"**/*{ct}*.bam")
-        pysam.merge("-f", "-o", str(output_dir / f"{ct}.bam"), *[str(b) for b in bam_files])
+        pysam.merge("-f", "-o", str(output_dir / f"{ct}.bam"), "-@", str(threads), *[str(b) for b in bam_files])
